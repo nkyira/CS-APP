6dd0165e8480bf8aff4f0b4a56b149ae7f8fdfd0
nkyira <joshuadavis1028@gmail.com>
2026-05-29 18:29:40 +0200

    md mod

--- code/src/main.c
+++ code/src/main.c
@@ -7,23 +7,32 @@ int main( void ) {
     printf("figure 2.1 :\n");
 
     int x = 95;
-    printf("\t showbytes(%d) ->", x);
+    printf("\tshowbytes(%d) ->", x);
     show_int(x);
     
     float y = 3.14159;
-    printf("\t showbytes(%f) ->", y);
+    printf("\tshowbytes(%f) ->", y);
     show_float(y);
 
     void *p = &y;
-    printf("\t showbytes(%p) ->", p);
+    printf("\tshowbytes(%p) ->", p);
     show_pointer(p);
 
 
     printf("example 2.1.7 :\n");
     int a = 15;
     int b = 20;
-    printf("\t %d AND %d = %d\n",a, b, and_on_int(a, b));
-    printf("\t %d OR %d = %d\n",a, b, or_on_int(a, b));
-    printf("\t %d XOR %d = %d\n",a, b, xor_on_int(a, b));
-    printf("\t %d NOT = %d\n",a, not_on_int(a));
+    printf("\t%d AND %d = %d\n",a, b, and_on_int(a, b));
+    printf("\t%d OR %d = %d\n",a, b, or_on_int(a, b));
+    printf("\t%d XOR %d = %d\n",a, b, xor_on_int(a, b));
+    printf("\t%d NOT = %d\n",a, not_on_int(a));
+
+    printf("example 2.2.4 :\n");
+    int  v = -12345;
+    unsigned int uv = (unsigned int)v;
+    printf("\tshowbytes(%d) ->", v);
+    show_int(v);
+    printf("\tshowbytes(%u) ->", uv);
+    show_int(uv);
+    printf("\tv = %d, uv = %u\n", v, uv);
 }
